e445bffa3a2a5c0bb82b058280f90fc9a18a4864
Mohsin Virk <mohsinlatifvirk@gmail.com>
2022-09-23 10:33:44 +0500

    update aesthetics a bit

--- src/components/@pages/movie/index.tsx
+++ src/components/@pages/movie/index.tsx
@@ -24,6 +24,7 @@ const Movie = () => {
       <div className='absolute top-4'>
         <GoBack />
       </div>
+
       <div className='grid grid-cols-1 md:grid-cols-2'>
         <img
           className='h-96 w-auto flex mx-auto md:ml-auto md:mr-0  object-cover lg:h-full'
@@ -37,6 +38,7 @@ const Movie = () => {
               <h2 className='leading-6 text-indigo-600 font-semibold tracking-wide uppercase'>
                 {year} - {director}
               </h2>
+
               <h3 className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
                 {title}
               </h3>
@@ -44,6 +46,7 @@ const Movie = () => {
 
               <div className='pt-4 md:pt-6'>
                 <h3 className='font-bold text-lg lg:2xl pb-1'>Genres</h3>
+
                 <div className='flex gap-x-2'>
                   {genres.map((g) => (
                     <Badge key={g} badge={g.toLowerCase()} />
@@ -53,11 +56,13 @@ const Movie = () => {
 
               <div className='pt-4 md:pt-6'>
                 <h3 className='font-bold text-lg lg:2xl'>Actors</h3>
+
                 <p className='text-base text-gray-500'>{actors}</p>
               </div>
 
               <div className='pt-4 md:pt-6'>
                 <h3 className='font-bold text-lg lg:2xl'>Run Time</h3>
+                
                 <p className='text-base text-gray-500'>{runtime} minutes</p>
               </div>
             </div>
--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -29,6 +29,7 @@ export class Search extends Component<Props> {
         <label htmlFor='simple-search' className='sr-only'>
           Search
         </label>
+
         <div className='relative w-full'>
           <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
             <svg
@@ -45,6 +46,7 @@ export class Search extends Component<Props> {
               />
             </svg>
           </div>
+
           <input
             type='text'
             id='simple-search'
@@ -55,6 +57,7 @@ export class Search extends Component<Props> {
             required
           />
         </div>
+
         <button
           type='submit'
           className='p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
@@ -73,6 +76,7 @@ export class Search extends Component<Props> {
               d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
             />
           </svg>
+          
           <span className='sr-only'>Search</span>
         </button>
       </form>
